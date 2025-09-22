3f860b5cf7e9a875d1d90413a21e0ce84374410e
Murtaza_Tanveer <murtaza.tanveer488@gmail.com>
2025-09-23 00:10:01 +0500

    Support Vector Classification.

---
+++ Classification/Support_Vector_Classification/SVC.py
@@ -0,0 +1,83 @@
+# Importing the Libraries
+import pandas as pd;
+import numpy as np;
+import matplotlib.pyplot as plt;
+
+# Importing the Dataset
+
+dataset = pd.read_csv("Social_Network_Ads.csv");
+
+# Data Cleaning 
+
+# print(dataset.info());
+
+# print(dataset.isnull().sum());
+
+print(dataset.corr(numeric_only=True));
+
+# Removing Outliars
+
+def removing_outliers_IQR(data, cols):
+    mask = pd.Series(True, index=data.index)  
+    
+    for c in cols:
+        Q1 = data[c].quantile(0.25)
+        Q3 = data[c].quantile(0.75)
+        IQR = Q3 - Q1
+        lowerBound = Q1 - 1.5 * IQR
+        upperBound = Q3 + 1.5 * IQR
+    
+        mask &= (data[c] >= lowerBound) & (data[c] <= upperBound)
+    
+    return data[mask]
+
+print(len(dataset))
+dataset = removing_outliers_IQR(dataset,['Age','EstimatedSalary']);
+print(len(dataset))
+
+# Splitting DataSet into Independent Variables (x) and Dependent Variables (y)
+
+x = dataset.iloc[:,:-1].values;
+y = dataset.iloc[:,-1].values;
+
+# Splitting the Dataset into the Training Set and Test Set
+
+from sklearn.model_selection import train_test_split;
+
+x_train , x_test , y_train , y_test = train_test_split(x, y, test_size=0.2, random_state=0);
+
+# Feature Scaling
+
+from sklearn.preprocessing import StandardScaler;
+
+sc = StandardScaler();
+x_train = sc.fit_transform(x_train) 
+x_test = sc.transform(x_test)
+x = sc.transform(x); 
+
+# Training the SVC model on the Training set
+
+from sklearn.svm import SVC;
+classifier = SVC(kernel="linear" , random_state=0);
+classifier.fit(x_train , y_train);
+
+# Predicting a new result
+
+print(classifier.predict(sc.transform([[30,87000]])));
+
+# Predicting the Test set results
+
+# y_pred = classifier.predict(x_test);
+
+# print(np.concatenate((y_test.reshape(-1,1), y_pred.reshape(-1,1)), axis=1));
+
+# Evaluating the Model Performance
+
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from classification_suitability_matrices import classification_suitability_Parameters;
+classification_suitability_Parameters(x,y, x_train, y_train, x_test, y_test, classifier);
+
