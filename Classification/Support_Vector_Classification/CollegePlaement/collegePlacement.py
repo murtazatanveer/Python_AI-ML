17c826d86420c1cb249159ecfb32e2ae0200fbf4
Murtaza_Tanveer <murtaza.tanveer488@gmail.com>
2025-09-23 19:28:25 +0500

    Applying SVC on Heart Disease and College Placement Dataset.

---
+++ Classification/Support_Vector_Classification/CollegePlaement/collegePlacement.py
@@ -0,0 +1,103 @@
+# Importing the Libraries
+
+import pandas as pd;
+import numpy as np;
+import matplotlib.pyplot as plt;
+
+# Importing the Dataset
+
+dataset = pd.read_csv("CollegePlacement.csv");
+
+# Data Cleaning
+
+dataset.drop(columns=['College_ID', 'Prev_Sem_Result'], inplace=True);
+
+# print(dataset.info());
+
+# print(dataset.isnull().sum());
+
+# print(dataset.corr(numeric_only=True));
+
+
+# Removing Outliars
+
+def removing_outliers_IQR(data, cols):
+    mask = pd.Series(True, index=data.index)  
+    
+    for c in cols:
+        Q1 = data[c].quantile(0.25)
+        Q3 = data[c].quantile(0.75)
+        IQR = Q3 - Q1
+        lowerBound = Q1 - 1.5 * IQR
+        upperBound = Q3 + 1.5 * IQR
+    
+        mask &= (data[c] >= lowerBound) & (data[c] <= upperBound)
+    
+    return data[mask]
+
+print(len(dataset))
+dataset = removing_outliers_IQR(dataset,['IQ','CGPA','Academic_Performance','Extra_Curricular_Score','Communication_Skills','Projects_Completed']);
+print(len(dataset)) 
+
+# Splitting DataSet into Independent Variables (x) and Dependent Variables (y)
+
+
+x = dataset.iloc[:,:-1].values;
+
+y = dataset.iloc[:,-1].values;
+
+# Encoding the Independent Variable
+
+from sklearn.compose import ColumnTransformer;
+from sklearn.preprocessing import OneHotEncoder;
+
+ct = ColumnTransformer(
+
+    transformers=[
+        ("Country Encoding", OneHotEncoder(), [3])
+    ],
+    remainder="passthrough"
+);
+
+x = np.array(ct.fit_transform(x));
+
+noise = np.random.normal(0, 0.5, size=x.shape)
+x = x + noise
+
+# Encoding the Dependent Variable
+
+from sklearn.preprocessing import LabelEncoder;
+
+le = LabelEncoder();
+y=le.fit_transform(y);
+
+# Splitting the Dataset into the Training Set and Test Set
+
+from sklearn.model_selection import train_test_split;
+
+x_train , x_test , y_train , y_test = train_test_split(x, y, test_size=0.2, random_state=0);
+
+# Feature Scaling
+
+from sklearn.preprocessing import StandardScaler;
+
+sc = StandardScaler();
+x_train = sc.fit_transform(x_train) 
+x_test = sc.transform(x_test)
+x = sc.transform(x); 
+
+
+# Training the SVM Classification model on the Training set
+
+from sklearn.svm import SVC;
+classifier = SVC(kernel="rbf", probability=True, random_state=0);
+classifier.fit(x_train, y_train);
+
+# Evaluating the Model Performance
+
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
+
+from classification_suitability_matrices import classification_suitability_Parameters
+
+classification_suitability_Parameters(x,y, x_train, y_train, x_test, y_test, classifier);
--- Classification/Support_Vector_Classification/Heart_Disease_Prediction/heartDiseasePrediction.py
+++ Classification/Support_Vector_Classification/Heart_Disease_Prediction/heartDiseasePrediction.py
@@ -1,19 +1,18 @@
 # Importing the Libraries
+
 import pandas as pd;
 import numpy as np;
 import matplotlib.pyplot as plt;
 
 # Importing the Dataset
 
-dataset = pd.read_csv("Social_Network_Ads.csv");
+dataset = pd.read_csv("heart.csv"); 
 
-# Data Cleaning 
+# Data Cleaning
 
 # print(dataset.info());
 
 # print(dataset.isnull().sum());
-
-print(dataset.corr(numeric_only=True));
 
 # Removing Outliars
 
@@ -32,7 +31,7 @@ def removing_outliers_IQR(data, cols):
     return data[mask]
 
 print(len(dataset))
-dataset = removing_outliers_IQR(dataset,['Age','EstimatedSalary']);
+dataset = removing_outliers_IQR(dataset,['Age','RestingBP',]);
 print(len(dataset))
 
 # Splitting DataSet into Independent Variables (x) and Dependent Variables (y)
@@ -40,11 +39,34 @@ print(len(dataset))
 x = dataset.iloc[:,:-1].values;
 y = dataset.iloc[:,-1].values;
 
+# Encoding the Independent Variable
+
+from sklearn.compose import ColumnTransformer;
+from sklearn.preprocessing import OneHotEncoder;
+
+ct = ColumnTransformer(
+
+    transformers=[
+        ("Country Encoding", OneHotEncoder(), [1,2,6,8,10])
+    ],
+    remainder="passthrough"
+);
+
+x = np.array(ct.fit_transform(x));
+
+# Encoding the Dependent Variable
+
+from sklearn.preprocessing import LabelEncoder;
+
+le = LabelEncoder();
+y=le.fit_transform(y);
+
 # Splitting the Dataset into the Training Set and Test Set
 
 from sklearn.model_selection import train_test_split;
 
 x_train , x_test , y_train , y_test = train_test_split(x, y, test_size=0.2, random_state=0);
+
 
 # Feature Scaling
 
@@ -55,29 +77,17 @@ x_train = sc.fit_transform(x_train)
 x_test = sc.transform(x_test)
 x = sc.transform(x); 
 
-# Training the SVC model on the Training set
+# Training the SVM Classification model on the Training set
 
-from sklearn.svm import SVC
-classifier = SVC(kernel="linear", random_state=0);
-classifier.fit(x_train , y_train);
-
-# Predicting a new result
-
-print(classifier.predict(sc.transform([[30,87000]])));
-
-# Predicting the Test set results
-
-# y_pred = classifier.predict(x_test);
-
-# print(np.concatenate((y_test.reshape(-1,1), y_pred.reshape(-1,1)), axis=1));
+from sklearn.svm import SVC;
+classifier = SVC(kernel="linear", probability=True, random_state=0);
+classifier.fit(x_train, y_train);
 
 # Evaluating the Model Performance
 
-import sys
-import os
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from classification_suitability_matrices import classification_suitability_Parameters
 
-from classification_suitability_matrices import classification_suitability_Parameters;
 classification_suitability_Parameters(x,y, x_train, y_train, x_test, y_test, classifier);
-
